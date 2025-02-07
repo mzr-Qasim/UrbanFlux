22a55882b5cc093a328a85c069c5f5821d8c1d7f
Muhammad Qasim <malikqasim20051@gmail.com>
2025-02-07 12:34:07 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,30 @@ var swiper = new Swiper(".mySwiper", {
   //   disableOnInteraction: false,
   // },
 });
+
+
+
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
