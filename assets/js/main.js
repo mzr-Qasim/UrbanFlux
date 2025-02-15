36d0243fead28ef9805746203fc962ab896dae0a
Muhammad Qasim <malikqasim20051@gmail.com>
2025-02-15 11:18:05 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,7 +126,7 @@ var mySwiper_related_product = new Swiper(".mySwiper_related_products", {
 
 
 document.getElementById('filter_close_button').addEventListener('click', function () {
-  document.getElementById('filters-sidebar').classList.toggle('closed');
+  document.getElementById('filters-sidebar').classList.toggle('open');
 });
 
 document.getElementById('filter_button').addEventListener('click', function () {
